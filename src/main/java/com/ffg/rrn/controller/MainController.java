0efa82daa22506381d2bc5fd6e05e5ace82f69b5
zhjjava <zhjjava@126.com>
2019-03-21 00:56:15 -0400

    Enhanced validation, error handling, disabled double submission
    Enhanced validation, error handling, disabled double submission

--- src/main/java/com/ffg/rrn/controller/MainController.java
+++ src/main/java/com/ffg/rrn/controller/MainController.java
@@ -33,7 +33,7 @@ import com.ffg.rrn.utils.WebUtils;
  */
 @Controller
 public class MainController {
-	
+
 	@Autowired
 	private ResidentServiceImpl residentService;
 
@@ -77,71 +77,6 @@ public class MainController {
 		
 		return "redirect:/getResidentById?residentId="+resident.getResidentId();		
 	}
-
-	@RequestMapping(value = "/getResidentById",  method = { RequestMethod.GET, RequestMethod.POST })
-	public String residents(@RequestParam("residentId") Long residentId, Model model, Principal principal) throws Exception{
-
-		// (1) (en)
-		// After user login successfully.
-		String serviceCoord = null;
-		if (principal != null) {
-			serviceCoord = populateSCinModel(model, principal);
-		}		
-		
-		Resident resident  = residentService.getResidentById(residentId, serviceCoord);
-		resident = residentService.getAllQuestionnaire(resident);		
-				
-		model.addAttribute("resident", resident);		
-		model.addAttribute("message", "Save new resident first or load existing Resident from All Resident Tab.");
-		
-		return "residentPage";
-		
-	}
-	
-	@RequestMapping(value = "/newResident", method = RequestMethod.GET)
-	public String residents(Model model, Principal principal) throws Exception{
-
-		// (1) (en)
-		// After user login successfully.
-		String serviceCoord = null;
-		if (principal != null) {
-			serviceCoord = populateSCinModel(model, principal);
-		}
-					
-		Resident resident  = residentService.getResidentById(0l, serviceCoord);
-		resident = residentService.getAllQuestionnaire(resident);
-				
-		model.addAttribute("resident", resident);
-		model.addAttribute("message", "Please select resident from All Resident Table first");
-		
-		return "residentPage";
-		
-	}
-	
-	@RequestMapping(value = "/allResident", method = RequestMethod.GET)
-	public String getAllResidents(Model model, Principal principal) throws Exception{
-
-		// (1) (en)
-		// After user login successfully.
-		
-		if (principal != null) {
-			populateSCinModel(model, principal);
-		}
-		
-		return "allResident";		
-	}
-	
-	@PostMapping("/saveResident")
-    public String signup(@Valid @ModelAttribute Resident resident, BindingResult bindingResult) {      
-        
-		if (bindingResult.hasErrors()) {
-            return "residentPage";
-        }
-		//This will be new ResidentId always
-        Long residentId = residentService.saveResident(resident);    
-        
-        return "allResident";
-    }
 	
 	@PostMapping("/saveHousing")
     public String saveHousingAssessment(@Valid @ModelAttribute Resident resident, BindingResult bindingResult) {      
