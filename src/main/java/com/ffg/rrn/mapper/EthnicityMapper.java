7f0b6da592c97e05e826cfb9a801eab6d2100606
frankristin <emailfranki5@gmail.com>
2020-06-09 09:13:45 -0400

    Create EthnicityMapper.java

---
+++ src/main/java/com/ffg/rrn/mapper/EthnicityMapper.java
@@ -0,0 +1,37 @@
+package com.ffg.rrn.mapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.ffg.rrn.model.DemographicChoice;
+
+/**
+ * @author FFGRRNTeam
+ *
+ */
+
+public class EthnicityMapper implements RowMapper<DemographicChoice> {
+	
+	public static final String ETHCTY_SQL //
+	= "select choice_id, choice from DEMOGRAPHICS_CHOICES where type='Ethnicity'"; 
+
+@Override
+public DemographicChoice mapRow(ResultSet rs, int row) throws SQLException {
+
+	DemographicChoice r = new DemographicChoice();
+
+r.setChoiceId(rs.getInt("choice_id"));
+r.setChoiceValue(rs.getString("choice"));
+
+return r;
+}
+
+}
+
+
+
+
+
+
+
