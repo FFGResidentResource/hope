5ce948356482b6033e1bccd62beab05a06087a61
rishabh <risha@DESKTOP-P9H6Q1P>
2020-08-29 14:18:31 -0400

    Reports

--- src/main/resources/public/js/dashboard.js
+++ src/main/resources/public/js/dashboard.js
@@ -1,21 +1,28 @@
+var properties = [];
+var oneTimeToggle = false;
 jQuery(document).ready(function() {
-
-    var now = new Date();
-    var year = now.getFullYear();
-    var ddl = document.getElementById("ddlYear");
-    for (var y = 0; y < 5; y++) {
-	var opt = document.createElement("option");
-	opt.value = (year - y).toString();
-	opt.innerText = (year - y).toString();
-	ddl.appendChild(opt);
-    }
-
-    var quarter = Math.floor((now.getMonth() + 3) / 3);
-    jQuery("#ddlQuarter").val(quarter);
+		
+    jQuery("[name^='_propId_']").each(function (key, value){
+		properties.push(value.id);
+	});
+	console.log(properties);	
     
-    jQuery("#_yearHeading").text(jQuery("#ddlYear").val());   
-
 });
+
+function addPropertyToReport(that){
+	
+	if(oneTimeToggle == false){
+		jQuery("[name^='_propId_']").removeClass("btn-primary").addClass("btn-default");
+		oneTimeToggle = true;
+		properties = [];
+	}
+	
+	properties.push(that.id);
+	jQuery("#"+that.id).removeClass("btn-default").addClass("btn-primary");
+	return false;		
+}
+
+
 
 
 function pullDashboard(){    
